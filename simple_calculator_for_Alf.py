ef0347c624225c1cfbb41dd6e6bf727747d376d0
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 16:54:42 +0800

    pseudo code

--- simple_calculator_for_Alf.py
+++ simple_calculator_for_Alf.py
@@ -1,1 +1,5 @@
 # ========= PSEUDO CODE ==========
+# - Printing 'Simple Calculator for Alf'
+# - Code for asking the user's input
+# - Code for performing the calculation based on user's inputted values 
+# - Code for displaying the output
