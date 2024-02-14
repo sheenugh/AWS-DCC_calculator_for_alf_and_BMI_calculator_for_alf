c3dde3ab0b30fd2a196ec596b184d958e6cdae0a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 17:43:45 +0800

    pseudo code

--- BMI_calculator_for_Alf.py
+++ BMI_calculator_for_Alf.py
@@ -1,1 +1,21 @@
 # ========= PSEUDO CODE ==========
+# BMI - NUTRITIONAL STATUS GUIDE
+#   BMI         NUTRITIONAL STATUS
+
+# BELOW 18.5         UNDERWEIGHT
+# 18.5 - 24.9       NORMAL WEIGHT
+# 25.0 - 29.9        OVERWEIGHT
+# ABOVE 30.0          OBESITY
+
+# - Asking the user's weight and height
+# - Calculations of user's BMI and categorize its status
+# - Displaying the user's BMI and its classification
+
+
+
+
+# Be cautious when reading input of various data types.
+# Select and employ a string concatenation method based on your personal preference and comfort level.
+
+
+# Use :.2f format specifier when printing the BMI value to display the BMI with only two decimal places.
